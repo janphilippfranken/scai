0d8773a64e01f662ce5963983bde1e44114a8759
Jan-Philipp Fränken <janphilipp.franken@gmail.com>
2023-05-30 14:49:16 -0700

    buffer

--- src/scai/modules/meta_prompt/base.py
+++ src/scai/modules/meta_prompt/base.py
@@ -13,6 +13,4 @@ from scai.modules.memory.buffer import CustomConversationBufferWindowMemory
 memory = CustomConversationBufferWindowMemory(k=1)
 memory.save_context({"system": "you are a helpful ai assistant"}, {"user": "hi"}, {"assistant": "how are you"})
 
-
-
-print(memory)
+print(memory.load_memory_variables())
