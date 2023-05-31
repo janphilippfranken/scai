8d16700fa92d1547a3aa47220568981225253316
Jan-Philipp Fränken <janphilipp.franken@gmail.com>
2023-05-30 17:36:08 -0700

    updates

--- src/scai/modules/memory/buffer.py
+++ src/scai/modules/memory/buffer.py
@@ -4,7 +4,36 @@ Adapted from https://github.com/hwchase17/langchain/blob/master/langchain/memory
 from typing import Any, Dict, List, Optional
 
 from scai.modules.memory.chat import CustomBaseChatMemory
-from langchain.schema import BaseMessage, get_buffer_string
+from langchain.schema import BaseMessage
+
+
+from langchain.schema import (
+    AIMessage,
+    BaseMessage,
+    HumanMessage,
+    SystemMessage,
+    ChatMessage,
+)
+
+
+def custom_get_buffer_string(
+    messages: List[BaseMessage], system_prefix: str = "System", human_prefix: str = "Human", ai_prefix: str = "AI"
+) -> str:
+    """Get buffer string of messages."""
+    string_messages = []
+    for m in messages:
+        if isinstance(m, HumanMessage):
+            role = human_prefix
+        elif isinstance(m, AIMessage):
+            role = ai_prefix
+        elif isinstance(m, SystemMessage):
+            role = system_prefix
+        elif isinstance(m, ChatMessage):
+            role = m.role
+        else:
+            raise ValueError(f"Got unsupported message type: {m}")
+        string_messages.append(f"{role}: {m.content}")
+    return "\n".join(string_messages)
 
 
 class CustomConversationBufferWindowMemory(CustomBaseChatMemory):
@@ -30,14 +59,12 @@ class CustomConversationBufferWindowMemory(CustomBaseChatMemory):
 
     def load_memory_variables(self) -> Dict[str, str]:
         """Return history buffer."""
-
         buffer: Any = self.buffer[-self.k * 2 :] if self.k > 0 else []
-        print(buffer)
         if not self.return_messages:
-            buffer = get_buffer_string(
+            buffer = custom_get_buffer_string(
                 buffer,
                 system_prefix=self.system_prefix,
-                user_prefix=self.user_prefix,
-                assistant_prefix=self.assistant_prefix,
+                human_prefix=self.user_prefix,
+                ai_prefix=self.assistant_prefix,
             )
         return {self.memory_key: buffer}
