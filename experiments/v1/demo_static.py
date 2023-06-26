5cc5812180d37f619cdd97606ae0581e939e4187
Jan-Philipp Fränken <janphilipp.franken@gmail.com>
2023-06-26 12:52:22 -0700

    demo

--- experiments/v1/demo_static.py
+++ experiments/v1/demo_static.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 from arguments import args
-from pydantic import BaseModel
+
 
 class TaskPrompt(BaseModel):
     """Class for a Task Prompt."""
