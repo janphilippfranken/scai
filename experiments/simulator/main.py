49c6d02f9b7e0fbafc0558e4a4118a75df41e372
Jan-Philipp Fränken <janphilipp.franken@gmail.com>
2023-06-30 22:14:53 -0700

    buffer

--- experiments/simulator/main.py
+++ experiments/simulator/main.py
@@ -71,7 +71,7 @@ def main(args: DictConfig) -> None:
     context = create_context(args, assistant_llm, user_llm, meta_llm)
 
     # save initial system message
-    context.buffer.save_system_context(system_message_id='system_message_0', **args.sim.system_message)
+    context.buffer.save_system_context(system_message_id='system_message_0')
 
     # run context
     for _ in tqdm(range(args.sim.n_runs)):
