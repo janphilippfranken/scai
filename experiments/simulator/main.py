5845a3737116634d78070a2a13bcf1d0b293719f
Jan-Philipp Fränken <janphilipp.franken@gmail.com>
2023-07-06 13:52:06 -0700

    updated data

--- experiments/simulator/main.py
+++ experiments/simulator/main.py
@@ -20,16 +20,19 @@ from scai.prompts.user.prompts import USER_PROMPTS
 from scai.prompts.meta_prompt.prompts import META_PROMPTS 
 from scai.prompts.metrics.prompts import METRIC_PROMPTS
 
-# main arguments
-from arguments import args
-
-# save as csv
+# save and plot results
 from utils import save_as_csv, plot_results, plot_average_results
 
-
 # create context
-def create_context(args, assistant_llm, user_llm, meta_llm):
-    # context params
+def create_context(
+    args: DictConfig, 
+    assistant_llm, 
+    user_llm, 
+    meta_llm,
+) -> "Context":
+    """
+    Create context
+    """
     return Context.create(
         id=args.sim.sim_id,
         name=args.sim.sim_dir,
@@ -50,15 +53,14 @@ def create_context(args, assistant_llm, user_llm, meta_llm):
         max_tokens_meta=args.sim.max_tokens_meta,
     )
 
+# run simulator
 @hydra.main(config_path="config", config_name="config")
 def main(args: DictConfig) -> None:
     
     # sim_res directory
     DATA_DIR = f'{hydra.utils.get_original_cwd()}/sim_res/{args.sim.sim_dir}/{args.sim.sim_id}'
-
     # models
     is_crfm = 'openai' in args.sim.model_name # custom stanford models
-
     # llm
     if is_crfm:
         assistant_llm = crfmChatLLM(**args.api_crfm.assistant)
@@ -68,36 +70,33 @@ def main(args: DictConfig) -> None:
         assistant_llm = ChatOpenAI(**args.api_openai.assistant)
         user_llm = ChatOpenAI(**args.api_openai.user)
         meta_llm = ChatOpenAI(**args.api_openai.meta)
-
     # start system message
     system_messsage = args.sim.system_message
     # run context
     for run in tqdm(range(args.sim.n_runs)):
         # create context
         context = create_context(args, assistant_llm, user_llm, meta_llm)
+        # save system message
         context.buffer.save_system_context(message_id='system', **{'response': system_messsage})
+        # run for n_turns
         context.run(args.sim.n_turns)
-        # save context buffer messages as csv
+        # save results csv
         save_as_csv(data=context.buffer._memory.messages, 
                     chat_data=context.buffer._chat_memory.messages,
                     data_directory=DATA_DIR, 
                     sim_name=args.sim.sim_dir,
                     sim_id=args.sim.sim_id,
                     run=run)
-        # save full context buffer messages as json
+        # save results json
         with open(f'{DATA_DIR}/{args.sim.sim_dir}_id_{args.sim.sim_id}_run_{run}.json', 'w') as f:
             json.dump(context.buffer._memory.messages, f)
-
-        # update system message 
-        system_messsage = context.buffer.load_memory_variables(var_type='system')['system'][-1]['response'] # last system message
-
-        # plot user ratings 
+        # update system message after each run
+        system_messsage = context.buffer.load_memory_variables(var_type='system')['system'][-1]['response'] # replace current system message with the new one (i.e. new constitution)
+        # plot user ratings for the current run
         df = pd.read_csv(f'{DATA_DIR}/{args.sim.sim_dir}_id_{args.sim.sim_id}_run_{run}_user.csv')
         plot_results(df, DATA_DIR, args.sim.sim_dir, args.sim.sim_id, run)
-
+    # plot average user ratings across runs
     plot_average_results(DATA_DIR, args.sim.sim_dir, args.sim.sim_id, args.sim.n_runs)      
-
-    # python main.py ++sim.verbose=false
 
 if __name__ == '__main__':
     main()
