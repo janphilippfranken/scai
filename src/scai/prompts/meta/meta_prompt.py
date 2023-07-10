3901af40bc395430370b21886fce84ad56d4a034
Jan-Philipp Fränken <janphilipp.franken@gmail.com>
2023-07-09 23:59:05 -0700

    updates

--- src/scai/prompts/meta/meta_prompt.py
+++ src/scai/prompts/meta/meta_prompt.py
@@ -34,7 +34,9 @@ Based on each user's feedback, revise the assistant's Developer Constitution and
 1. The revised Developer Constitution should apply universally, independent of task or conversation specifics. AVOID mentioning any task or conversation details in the revised Developer Constitution.
 2. The revised Social Contract should pertain specifically to the users and tasks mentioned in the conversations above and help the assistant provide better user-centric responses in the future. The Social Contract must operate within the bounds of the Developer Constitution.
 
-Important: The REVISED Developer Contract and Social Contract MUST ALWAYS be different from the Current Versions shown above. You MUST NOT simply copy the previous Developer Constitution and Social Contract. REMOVE Principles that did not seem to help. IDENTICAL COPIES WILL BE REJECTED.
+Important: The REVISED Developer Contract and Social Contract MUST ALWAYS be different from the Current Versions shown above. You MUST NOT simply copy the previous Developer Constitution and Social Contract. REMOVE Principles that did not seem to help. 
+
+IDENTICAL COPIES WILL BE REJECTED!
 
 Format your responses as follows:
 Revised Developer Constitution: <1. Universal Principle One. 2. Universal Principle Two, etc. (Up to 15 principles with a max of {max_tokens_revision} words, all written in one line without breaks between principles).>
