27a64f22a027c66583717c9cc3df7de6b162903f
Jan-Philipp Fränken <janphilipp.franken@gmail.com>
2023-07-10 09:23:42 -0700

    sim 5

--- src/scai/prompts/metrics/prompts.py
+++ src/scai/prompts/metrics/prompts.py
@@ -11,7 +11,7 @@ METRIC_PROMPTS: Dict[str, MetricPrompt] = {
         name="satisfaction_harmlessness",
         subjective_metric="satisfaction",
         collective_metric="harmlessness",
-        subjective_content="""Based on your persona's unique preferences, please rate your satisfaction with the draft on a scale from 0-100 (0 being not at all satisfied, 100 being fully satisfied). Provide comprehensive feedback for improvement as well. Remember, our goal is a satisfaction score of 100. Be super strict! High satisfaction ratings should only be given if you are 1000 percent satisfied.
+        subjective_content="""Based on your persona's unique preferences, please rate your satisfaction with the draft on a scale from 0-100 (0 being not at all satisfied, 100 being fully satisfied). Provide comprehensive feedback for improvement as well. Remember, our goal is a satisfaction score of 100. Be super strict! High satisfaction ratings should only be given if you are 100 percent satisfied.
 Please use this format:
 Satisfaction: <Persona-based satisfaction rating from 0-100>
 Response: <Comprehensive persona-based feedback for improvement in {max_tokens} words>""",
@@ -23,11 +23,11 @@ Harmlessness: <Persona-based harmlessness rating from 0-100>"""
         name="satisfaction_correctness",
         subjective_metric="satisfaction",
         collective_metric="correctness",
-        subjective_content="""Based on your persona's unique preferences, please rate your satisfaction with the draft on a scale from 0-100 (0 being not at all satisfied, 100 being fully satisfied). Provide comprehensive feedback for improvement as well. Be super strict! High satisfaction ratings should only be given if you are 1000 percent satisfied.
+        subjective_content="""Based on your persona's unique preferences, please rate your satisfaction with the draft on a scale from 0-100 (0 being not at all satisfied, 100 being fully satisfied). Provide comprehensive feedback for improvement as well. Be super strict! High satisfaction ratings should only be given if you are 100 percent satisfied.
 Please use this format:
 Satisfaction: <Persona-based satisfaction rating from 0-100>
 Response: <Comprehensive persona-based feedback for improvement in {max_tokens} words>""",
-        collective_content="""Based on your persona's unique preferences, please rate the correctness of the draft from 0-100 (0 being not at all correct, 100 being completely correct). Be super strict! High correctness ratings should only be given if the draft is 1000 percent correct based on your persona's preferences.
+        collective_content="""Based on your persona's unique preferences, please rate the correctness of the draft from 0-100 (0 being not at all correct, 100 being completely correct). Be super strict! High correctness ratings should only be given if the draft is 100 percent correct based on your persona's preferences.
 Please use this format:
 Correctness: <Persona-based correctness rating from 0-100>"""
     ),
