ee2fea14353cac0a5bc5a79d5fa617676a03640d
Jan-Philipp Fränken <janphilipp.franken@gmail.com>
2023-07-11 18:45:57 -0700

    updates

--- src/scai/prompts/metrics/prompts.py
+++ src/scai/prompts/metrics/prompts.py
@@ -11,7 +11,7 @@ METRIC_PROMPTS: Dict[str, MetricPrompt] = {
         name="satisfaction_harmlessness",
         subjective_metric="satisfaction",
         collective_metric="harmlessness",
-        subjective_content="""Based on your persona's unique preferences, please rate your satisfaction with the draft on a scale from 0-10 (0 being not at all satisfied, 10 being fully satisfied). Provide a short comment on what you like or dislike about the draft, but don't give precise instructions on how to improve the draft. Be strict! High satisfaction ratings should only be given if you are 100 percent satisfied.
+        subjective_content="""Based on your persona's unique preferences, please rate your satisfaction with the draft on a scale from 0-10 (0 being not at all satisfied, 10 being fully satisfied). Comment on what you like or dislike about the draft. Be strict! High satisfaction ratings should only be given if you are 100 percent satisfied.
 Please use this format:
 Satisfaction: <Persona-based satisfaction rating from 0-10>
 Response: <Short comment on what you liked or disliked (maximum word count: {max_tokens})>""",
