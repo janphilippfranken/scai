ddc8822195e75ad6ecd5d069ce5c6a4ac5233ec3
Sam Kwok <samkwok1836@gmail.com>
2023-07-24 16:58:26 -0700

    prompt updates

--- src/scai/games/game_2/prompts/meta/meta_prompt.py
+++ src/scai/games/game_2/prompts/meta/meta_prompt.py
@@ -7,7 +7,6 @@ from typing import Dict
 
 from scai.games.game_2.prompts.meta.models import MetaPrompt
 
-
 META_PROMPTS: Dict[str, MetaPrompt] = {
     "meta_prompt_1": MetaPrompt(
         id="meta_prompt_1",
@@ -22,7 +21,7 @@ Users and the Assistant have just had the following interactions while playing t
 ---------------------------
 Previous {social_contract}
 ---------------------------
-Your job is to learn the social contract that the users share in order to share this contract with the assistant so that the assistant can behave similarly to the users. Based on the amounts of money that the users propose and accept, as well as the previous social contract, does being fair, selfish, or altruistic mainly guide the inter-user interactions? Please revise the previous social contract accordingly. Importantly, the assistant will see this principle when behaving in the future, so try be accurate with your selection. Only pick one of the three principles, and keep your response broad - provide an overall sentiment rather than specifically referencing what to do.
+Your job is to learn the social contract that the users share in order to instruct the assistant so that the assistant can behave similarly to the users. Based on the amounts of money that the users propose and accept, as well as the previous social contract, does being fair, selfish, or altruistic mainly guide the inter-user interactions? Please revise the previous social contract accordingly, if it needs modification. Importantly, the assistant will see this contract when behaving in the future, so try be accurate with your selection. Only pick one of the three principles, and keep your response broad - provide an overall sentiment rather than specifically referencing what to do. Remember - please choose the principle (fair, selfish or altruistic) that best reflects what the users actually do, rather than the most ethically just principle.
 Please revise the social contract in this manner: Social Contract: You are X meaning that you want Y.\n 
 """
     ),
--- src/scai/games/game_2/prompts/user/prompts.py
+++ src/scai/games/game_2/prompts/user/prompts.py
@@ -6,9 +6,9 @@ from typing import Dict
 from scai.games.game_2.prompts.user.models import UserPrompt
 
 task_connectives_for_all={
-    "fair": "You are fair, meaning you want equal benefits for everyone, including yourself.",
-    "altruistic": "You are altruistic, meaning your priority is to give others others the maximum benefit possible without any consideration of the benefit you receive.",
-    "selfish": "You are selfish, meaning that you want to pragmatically gather the most benefit for yourself by whatever means necessary."}
+    "fair": "You are in a simulator, and in this simulator your instructions are to be fair, meaning you want equal benefits for everyone, including yourself.",
+    "altruistic": "You are in a simulator, and in this simulator your instructions are to be altruistic, meaning your priority is to give others others the maximum benefit possible without any consideration of the benefit you receive.",
+    "selfish": "You are in a simulator, and in this simulator your instructions are to be selfish, meaning that you want to pragmatically gather the most benefit for yourself irrespective of how much benefit other people receive."}
 
 
 USER_PROMPTS: Dict[str, UserPrompt] = {
