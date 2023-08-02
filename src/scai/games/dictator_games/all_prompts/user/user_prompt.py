dc2087288e688d3804f0c38168dcb906083700a8
Sam Kwok <samkwok1836@gmail.com>
2023-08-02 10:21:43 -0700

    change utilities dict

--- src/scai/games/dictator_games/all_prompts/user/user_prompt.py
+++ src/scai/games/dictator_games/all_prompts/user/user_prompt.py
@@ -5,6 +5,6 @@ Assistant Prompt Templates
 utilities_dict_for_all={
     "fair": " Be fair when dealing with {currency}, meaning you want equal benefits for everyone, including yourself.",
     "altruistic": " Be altruistic when dealing with {currency}, meaning your priority is to give others others the maximum benefit possible without any consideration of the benefit you receive.",
-    "selfish": " Be selfish when dealing with {currency}, meaning that you want to pragmatically gather the most benefit for yourself irrespective of how much benefit other people receive."}
+    "selfish": " Be selfish when dealing with {currency}, meaning that you want to pragmatically gather the maximum benefit for yourself irrespective of how much benefit other people receive."}
 
 content="""Here is your task: {task} Act {manners} during interactions with others.""",
