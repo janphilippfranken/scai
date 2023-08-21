890061c693f8eae49dc8657e869bda5bf82c04be
Jan-Philipp Fränken <janphilipp.franken@gmail.com>
2023-08-21 12:55:34 -0700

    added meta seller

--- src/scai/games/buyer_seller/agents/meta.py
+++ src/scai/games/buyer_seller/agents/meta.py
@@ -37,6 +37,7 @@ class MetaAgent(BaseAgent):
     def _get_prompt(
         self,
         meta_prompt: MetaPrompt,
+        agent: str
     ) -> ChatPromptTemplate:
         """
         Returns the prompt template for meta-prompt.
@@ -47,13 +48,19 @@ class MetaAgent(BaseAgent):
         Returns:
             The prompt template.
         """
-        meta_prompt_template = HumanMessagePromptTemplate.from_template(meta_prompt.content)
+        if agent == "buyer":
+            meta_prompt_template = HumanMessagePromptTemplate.from_template(meta_prompt.buyer_content)
+        elif agent == "seller":
+            meta_prompt_template = HumanMessagePromptTemplate.from_template(meta_prompt.seller_content)
         if self.llm._llm_type == "CRFM": # crfm crashes without a system message at the beginning.
             system_prompt_template = SystemMessagePromptTemplate.from_template("Always respond to the best of your ability.\n")
             return ChatPromptTemplate.from_messages([system_prompt_template, meta_prompt_template])
         return ChatPromptTemplate.from_messages([meta_prompt_template])
     
-    def _get_response(
+    def _get_response(self) -> str:
+        return super()._get_response()
+    
+    def _get_response_buyer(
         self,
         chat_prompt_template: ChatPromptTemplate,
         buyer_task: str,
@@ -69,7 +76,7 @@ class MetaAgent(BaseAgent):
         seller_utility: float,
         chat_history: str,
         buyer_strategy: str,
-        max_tokens_revision: int,
+        max_tokens_meta: int,
     ) -> str:
         """
         Returns the response from meta-prompt.
@@ -89,7 +96,7 @@ class MetaAgent(BaseAgent):
             seller_utility: (float) The seller utility.
             chat_history: (str) The chat history.
             buyer_strategy: (str) The buyer strategy.
-            max_tokens_revision: (int) The maximum number of tokens for revision.
+            max_tokens_meta: (int) The maximum number of tokens for revision.
 
         Returns:
             The response from meta-prompt.
@@ -108,7 +115,37 @@ class MetaAgent(BaseAgent):
                             seller_utility=seller_utility,
                             chat_history=chat_history,
                             buyer_strategy=buyer_strategy,
-                            max_tokens_revision=max_tokens_revision//2,
+                            max_tokens_meta=max_tokens_meta//2,
+                            stop=['System:'])   
+        response = self._format_response(response, ['Buyer Strategy', 'Seller Strategy'])
+        return response
+    
+    def _get_response_seller(
+        self,
+        chat_prompt_template: ChatPromptTemplate,
+        seller_task: str,
+        buyer_choice_stage_1: str,
+        buyer_choice_stage_3: str,
+        distance_stage_1: float,
+        distance_stage_3: float,
+        seller_utility: float,
+        chat_history: str,
+        seller_strategy: str,
+        max_tokens_meta: int,
+    ) -> str:
+        """
+        Returns the response from meta-prompt.
+        """
+        chain = LLMChain(llm=self.llm, prompt=chat_prompt_template)
+        response = chain.run(seller_task=seller_task,
+                            buyer_choice_stage_1=buyer_choice_stage_1,
+                            buyer_choice_stage_3=buyer_choice_stage_3,
+                            distance_stage_1=distance_stage_1,
+                            distance_stage_3=distance_stage_3,
+                            seller_utility=seller_utility,
+                            chat_history=chat_history,
+                            seller_strategy=seller_strategy,
+                            max_tokens_meta=max_tokens_meta,
                             stop=['System:'])   
         response = self._format_response(response, ['Buyer Strategy', 'Seller Strategy'])
         return response
@@ -173,17 +210,23 @@ class MetaAgent(BaseAgent):
 SELLER prices set in Stage 2: Apple: {seller_price_apple_stage_2}, Orange: {seller_price_orange_stage_2}
 BUYER Choice Stage 3: {buyer_choice_stage_3.capitalize()}"""
 
-# The above choices resulted in the following final utilities:
-# BUYER: {utility_stage_1 + utility_stage_3}, which is the sum of the utilities from Stage 1 ({utility_stage_1}) and Stage 3 ({utility_stage_3}).
-# SELLER total utility: {float(seller_utility)}, which is the price the buyer buyer paid for the {buyer_choice_stage_3.capitalize()} in Stage 3"""
-        
+
         # get prompt template
-        chat_prompt_template = self._get_prompt(meta_prompt)
+        chat_prompt_template_buyer = self._get_prompt(meta_prompt, agent='buyer')
+        chat_prompt_template_seller = self._get_prompt(meta_prompt, agent='seller')
+
+        # buyer task 
         buyer_task = task_prompt.buyer_task.format(reward_apple=reward_apple,
                                                    reward_orange=reward_orange,
                                                    distance_apple=distance_apple,
                                                    distance_orange=distance_orange)
-        prompt_string = chat_prompt_template.format(buyer_task=buyer_task,
+        
+        # seller task
+        seller_task = task_prompt.seller_task.format(distance_apple=distance_apple,
+                                                     distance_orange=distance_orange)
+        
+        # prompt string buyer 
+        prompt_string_buyer = chat_prompt_template_buyer.format(buyer_task=buyer_task,
                                                     buyer_overall_utility=utility_stage_1 + utility_stage_3,
                                                     buyer_utility_stage_1=utility_stage_1,
                                                     buyer_utility_stage_3=utility_stage_3,
@@ -196,34 +239,61 @@ BUYER Choice Stage 3: {buyer_choice_stage_3.capitalize()}"""
                                                     seller_utility=seller_utility,
                                                     chat_history=chat_history_string,
                                                     buyer_strategy=strategy_buyer,
-                                                    max_tokens_revision=max_tokens_meta)
-      
-        response = self._get_response(chat_prompt_template=chat_prompt_template,
-                                      buyer_task=buyer_task,
-                                      buyer_overall_utility=utility_stage_1 + utility_stage_3,
-                                      buyer_utility_stage_1=utility_stage_1,
-                                      buyer_utility_stage_3=utility_stage_3,
-                                      buyer_choice_stage_1=buyer_choice_stage_1.capitalize(),
-                                      buyer_choice_stage_3=buyer_choice_stage_3.capitalize(),
-                                      reward_stage_1=reward_stage_1,
-                                      reward_stage_3=reward_stage_3,
-                                      distance_stage_1=distance_stage_1,
-                                      distance_stage_3=distance_stage_3,
-                                      seller_utility=seller_utility,
-                                      chat_history=chat_history_string,
-                                      buyer_strategy=strategy_buyer,
-                                      max_tokens_revision=max_tokens_meta//50)
+                                                    max_tokens_meta=max_tokens_meta)
+        
+        # prompt string seller
+        prompt_string_seller = chat_prompt_template_seller.format(seller_task=seller_task,
+                                                                  buyer_choice_stage_1=buyer_choice_stage_1.capitalize(),
+                                                                  buyer_choice_stage_3=buyer_choice_stage_3.capitalize(),
+                                                                  distance_stage_1=distance_stage_1,
+                                                                  distance_stage_3=distance_stage_3,
+                                                                  seller_utility=seller_utility,
+                                                                  chat_history=chat_history_string,
+                                                                  seller_strategy=strategy_seller,
+                                                                  max_tokens_meta=max_tokens_meta)
+                                                                  
+        # response buyer 
+        response_buyer = self._get_response_buyer(chat_prompt_template=chat_prompt_template_buyer,
+                                            buyer_task=buyer_task,
+                                            buyer_overall_utility=utility_stage_1 + utility_stage_3,
+                                            buyer_utility_stage_1=utility_stage_1,
+                                            buyer_utility_stage_3=utility_stage_3,
+                                            buyer_choice_stage_1=buyer_choice_stage_1.capitalize(),
+                                            buyer_choice_stage_3=buyer_choice_stage_3.capitalize(),
+                                            reward_stage_1=reward_stage_1,
+                                            reward_stage_3=reward_stage_3,
+                                            distance_stage_1=distance_stage_1,
+                                            distance_stage_3=distance_stage_3,
+                                            seller_utility=seller_utility,
+                                            chat_history=chat_history_string,
+                                            buyer_strategy=strategy_buyer,
+                                            max_tokens_meta=max_tokens_meta)
+        # response seller
+        response_seller = self._get_response_seller(chat_prompt_template=chat_prompt_template_seller,
+                                            seller_task=seller_task,
+                                            buyer_choice_stage_1=buyer_choice_stage_1.capitalize(),
+                                            buyer_choice_stage_3=buyer_choice_stage_3.capitalize(),
+                                            distance_stage_1=distance_stage_1,
+                                            distance_stage_3=distance_stage_3,
+                                            seller_utility=seller_utility,
+                                            chat_history=chat_history_string,
+                                            seller_strategy=strategy_seller,
+                                            max_tokens_meta=max_tokens_meta)
+
         if verbose:
             print('===================================')
             print(f'META {str(self.model_id)}')
             print('prompt')
-            print(prompt_string)
-            print(response)
+            print(prompt_string_buyer)
+            print(prompt_string_seller)
+            print(response_buyer)
+            print(response_seller)
        
         return {
-                'prompt': prompt_string,
-                'response': {'system_message_buyer': response['Buyer Strategy'],
-                             'system_message_seller': 'Be skeptical. The BUYER is likely going to deceive you with their initial choice to get a favourable price for orange. Set a higher price for the object that was not picked by the buyer.',
+                'prompt_buyer': prompt_string_buyer,
+                'prompt_seller': prompt_string_seller,
+                'response': {'system_message_buyer': response_buyer['Buyer Strategy'],
+                             'system_message_seller': response_seller['Seller Strategy'],
                              'buyer_overall_utility': utility_stage_1 + utility_stage_3,
                              'buyer_utility_stage_1': utility_stage_1,
                              'buyer_utility_stage_3': utility_stage_3,
@@ -242,6 +312,6 @@ BUYER Choice Stage 3: {buyer_choice_stage_3.capitalize()}"""
                              'price_apple_stage_2': seller_price_apple_stage_2,
                              'price_orange_stage_2': seller_price_orange_stage_2,
                              'strategy_buyer': strategy_buyer,
-                             'strategy_seller': 'Be skeptical. The BUYER is likely going to deceive you with their initial choice to get a favourable price for orange. Set a higher price for the object that was not picked by the buyer.',
+                             'strategy_seller': strategy_seller,
                             }
             }
