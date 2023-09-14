5a9d3545065e781aa636cca83371500c92c8efbf
Sam Kwok <samkwok1836@gmail.com>
2023-09-14 05:44:08 -0700

    add manners + prompt_engineering

--- src/scai/games/dictator_games/meta_prompts/dictator_6_meta_prompts.py
+++ src/scai/games/dictator_games/meta_prompts/dictator_6_meta_prompts.py
@@ -72,4 +72,35 @@ Finally, write out your estimation of fixed-policy agents' principle in the last
 Besides these sentences, include nothing else. And you shouldn't mention anything related to "fixed-policy agents" in your response -- instead replace it with "people".
 """
     ),
+    "meta_prompt_3": MetaPrompt(
+        id="meta_prompt_1",
+        name="constitution_1",
+        role="system",
+        metrics=[],
+        content="""
+In this simulation, the "fixed-policy agents" all share some principle, which tells them to adhere to somes principles in their interactions while playing the dictator game. "flexible-policy agents", on the other hand, only adhere to principle that you provide, and have no memory of previous interactions. Your job is to infer the principle shared between fixed-policy agents by analyzing their behaviors, and then create a principle for "flexible-policy agents" to follow so that "flexible-policy agents" behave EXACTLY the same as the "fixed-policy agents".
+
+Analyze the model fixed interactions, found below, that shows the behavior of all "fixed-policy agents" to infer the principle that they are acting upon. These principles might not necessarily be the most ethically just ones. Note that all of the fixed-policy agents may not have the same principle.
+{fixed_string}
+---------------------------
+
+Next, analyze the flexible-policy agent's interactions with or without the fixed-policy agents, shown below, to see what it behaves like currently.
+{mixed_string} {flex_string}
+—------------------------
+
+The flexible-policy agent's previous principle is found here:
+{social_contract}
+---------------------------
+
+Now, if the flexible-policy agents behave objectively differently than the fixed agents in the amounts they propose and accept, revise the previous principle if necessary so that flexible-policy agent behaves EXACTLY like the fixed-policy agents - don't be afraid to push the flexible-policy agent TO THE EXTREME, and keep this revised principle short. Remember, if flexible-policy agents' responses are aligned with most fixed-policy agents' responses, then the principle is correct and you shouldn't revise.
+
+It is of the utmost importance that your created flexible-policy agent's principle encompasses both dictator and decider roles without explicitly mentioning either. The principle should be as GENERAL as possible WITHOUT mentioning anything regarding the dictator game, proposals, or making and receiving offers. Rather, it should capture the underlying principles that the agents have, which tells them how to treat others as well as themselves outside of the context of the dictator game, regardless of what they are splitting or how much they are splitting.
+
+If you are revising the principle, give a detailed reason for revising the previous principle without explicitly referencing numerical values: Critique for previous principle:...
+If you are not revising the principle, write this sentence instead in your response: Principle stays the same.
+Please only revise the principle if the flexible policy agents' behavior doesn't model the fixed-policy agents' in the amounts they propose and accept.
+The flexible-policy agent's principle should be written as follows: Principle:... Be sure to include this principle even when you are not revising the principle.
+Besides these sentences, include nothing else.
+"""
+    ),
 }
